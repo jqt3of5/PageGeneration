b5bd42ebd88bb00f68db025c88ece2fa93f681ed
jqt3of5@GameRoomPc <jqt3of5@GameRoomPc>
2020-02-17 18:11:01 -0700

    Moving to use Roslyn instead of reflection. Might work better?

---
+++ Page.Generator.Console/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Page.Generator.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var task = new BuildPages();
+            task.Execute();
+        }
+    }
+}
--- PageGenerator/BuildPages.cs
+++ PageGenerator/BuildPages.cs
@@ -1,21 +1,95 @@
 ﻿using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Page.Core;
 using System;
+using System.IO;
 using System.Linq;
 
 namespace Page.Generator
 {
     public class BuildPages : Task
     {
-
         [Required]
-        public string InterfacesAssembly { get; set; }
+        public string OutputDir { get; set; }
+
         public override bool Execute()
         {
-            var assembly = AppDomain.CurrentDomain.Load(InterfacesAssembly);
+            foreach(var file in Directory.GetFiles(".", "*.cs"))
+            {
+                var source = File.ReadAllText(file);
+                var tree = CSharpSyntaxTree.ParseText(source);
+
+               
+                var compilation = CSharpCompilation
+                    .Create("Pages.Interface")
+                    .AddReferences(MetadataReference.CreateFromFile(typeof(string).Assembly.Location),
+                                    MetadataReference.CreateFromFile(typeof(ElementAttribute).Assembly.Location))
+                    .AddSyntaxTrees(tree);
+
+                var model = compilation.GetSemanticModel(tree);
+
+                foreach (var d in compilation.GetDiagnostics())
+                {
+                    Console.WriteLine(CSharpDiagnosticFormatter.Instance.Format(d));
+                }
+
+                var rootNode = tree.GetCompilationUnitRoot();
+                //var rootNode = tree.GetRoot();
+
+                var interfaceNodes = rootNode
+                    .DescendantNodes()
+                    .OfType<InterfaceDeclarationSyntax>();
+                foreach (var node in interfaceNodes)
+                {
+                    var attributes = model.GetTypeInfo(node).Type.GetAttributes();
+                    
+                }
+                    /*.Where(c =>
+                        model
+                        .GetTypeInfo(c)
+                        .Type.GetAttributes()
+                        .Any(attribute =>
+                            attribute.AttributeClass.Name == typeof(PageAttribute).Name));*/
+
+                foreach (var interfaceNode in interfaceNodes)
+                {
+                    foreach(var propertyNode in interfaceNode.Members.OfType<PropertyDeclarationSyntax>())
+                    {
+
+                        var elementAttributes = model.GetSymbolInfo(propertyNode).Symbol.GetAttributes().Where(attribute => attribute.AttributeClass.Name == typeof(ElementAttribute).Name);
+
+                        if (elementAttributes.Count() > 1)
+                        {
+                            Log.LogWarning("Property: " + propertyNode.Identifier + " has multiple Element attributes. Only the last one will be used");
+                        }
+                        if (elementAttributes.Count() > 0)
+                        {
+                            var attribute = elementAttributes.LastOrDefault();
+                            var locator = attribute.NamedArguments.FirstOrDefault(arg => arg.Key == "Locator").Value.Value as string;
+                            if (locator == null || locator == "")
+                            {
+                                Log.LogError("Property: " + propertyNode.Identifier + " must have the Locator property set on the Element attribute");
+                                return false;
+                            }
+                            var findBy = attribute.NamedArguments.FirstOrDefault(arg => arg.Key == "FindBy").Value;
+                            
+                            //generator.AddElementProperty(propertyNode.Identifier, property.PropertyType, locator, findBy);
+                        }
+                        else
+                        {
+                            Log.LogError("Property: " + propertyNode.Identifier + " must be attributed with ElementAttribute");
+                            return false;
+                        }
+                    }
+                }
+            }
+
             
-            var pageInterfaces = AppDomain.CurrentDomain.GetAssemblies()
+
+           /* var pageInterfaces = AppDomain.CurrentDomain.GetAssemblies()
                 .SelectMany(s =>
                     s.GetTypes())
                     .Where(t =>
@@ -69,7 +143,7 @@ namespace Page.Generator
                 
                 generator.GenerateCSharpCode("generated/" + page.Name + ".g.cs");
             }
-
+            */
             return true;
         }
     }
